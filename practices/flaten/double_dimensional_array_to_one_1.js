6c009f1f9450fa55384334df8ad7d19194a344f9
“FreyaCaiFuHan <568025639@qq.com>
2018-04-25 21:37:14 +0800

    finish flaten

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  let result = [1];
+  let count = 1;
+  for (let i = 0 ; i < collection.length ; i++){
+    for (let j = 0 ; j < collection[i].length ; j++){
+      result[count] = collection[i][j];
+      count++;
+    }
+  }
+  return result; 
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,28 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  let result = [];
+  let count = 0;
+  let temp = [];
+  for (let i = 0 ; i < collection.length ; i++){
+    for (let j = 0 ; j < collection[i].length ; j++){
+      temp[count] = collection[i][j];
+      count++;
+    }
+  }
+  result[0] = temp[0];
+  for (let m = 1 ; m < temp.length ; m++){
+    for (let n = 0 ; n < result.length ; n++){
+      if (temp[m] != result[n] && n == result.length-1){
+        result[n+1] = temp[m];
+      }
+      else if (temp[m] == result[n]){
+        break;
+      }
+    }
+    
+  }
+  return result; 
 }
 
 module.exports = double_to_one;

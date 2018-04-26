de6776b61404696f12c427e996ab282a44f51ecc
“FreyaCaiFuHan <568025639@qq.com>
2018-04-26 12:11:54 +0800

    finish ruduce

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+  let result = [];
+  let i = 0;
+  let interval10 = 10*interval
+  result[0] = number*10;
+  while (result[i] > 0){
+    result[i+1] = result[i]-interval10;
+    i++;
+  }
+  for (let i = 0; i< result.length; i++){
+    result[i] = result[i]/10;
+  }
+  return result;
 }
-
 module.exports = spilt_to_zero;
